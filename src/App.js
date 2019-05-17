4f844e053cbd449fda621fa167d0ee1c3eebcfd8
sumpaotong <tsumpaotong@dxc.com>
2019-05-17 13:39:24 +0700

    Add next button

--- src/App.js
+++ src/App.js
@@ -7,32 +7,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-			This site is used to practice myReact skill.
-        </p>
-		<p>
-			Creator : Thanakorn Sumpaotong
-		</p>
-		<a
-          className="App-link"
-          href="https://web.facebook.com/kapoat"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Facebook
-        </a>
-		<p>
-			Go to &nbsp;
-			<a
-			className="App-link"
-			href="https://github.com/TSoat/openProject"
-			target="_blank"
-			rel="noopener noreferrer"
-			>
-			git repository
-			</a>
-			&nbsp; of this site.
-		</p>
+          <p>This site is used to practice myReact skill.</p>
+		      <p>Creator : Thanakorn Sumpaotong</p>
+		      <a
+            className="App-link"
+            href="https://web.facebook.com/kapoat"
+            target="_blank"
+            rel="noopener noreferrer">
+            Facebook
+          </a>
+		      <p>Go to &nbsp;
+			       <a
+			          className="App-link"
+			          href="https://github.com/TSoat/openProject"
+			          target="_blank"
+			          rel="noopener noreferrer">
+			          git repository
+			       </a>
+			    &nbsp; of this site.</p>
+          <button id = "nextButton" class = "button">Next</button>
       </header>
     </div>
   );
